41f16d9a4b6c5c52a30ca1ea3b0aad3b2e7b070c
Joseph Maramba <joseph@smartmile.eu>
2022-08-22 12:48:42 +0300

    change api key to tmdb

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
 import { useEffect, useState } from 'react';
 import Footer from './components/Footer';
-import NavBar from './components/NavBar';
+import { Movie, NavBar } from './components';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
 	const [movieCharacter, setMovieCharacter] = useState('avengers');
 	const API_KEY = import.meta.env.VITE_API_KEY;
 
-	/* Shape of data  
-  {
-    "Title": "Batman v Superman: Dawn of Justice",
-    "Year": "2016",
-    "imdbID": "tt2975590",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-}  
-  */
+	// {
+	//   "adult": false,
+	//   "backdrop_path": "/7ZO9yoEU2fAHKhmJWfAc2QIPWJg.jpg",
+	//   "genre_ids": [
+	//     28,
+	//     878,
+	//     53
+	//   ],
+	//   "id": 766507,
+	//   "original_language": "en",
+	//   "original_title": "Prey",
+	//   "overview": "When danger threatens her camp, the fierce and highly skilled Comanche warrior Naru sets out to protect her people. But the prey she stalks turns out to be a highly evolved alien predator with a technically advanced arsenal.",
+	//   "popularity": 9839.591,
+	//   "poster_path": "/ujr5pztc1oitbe7ViMUOilFaJ7s.jpg",
+	//   "release_date": "2022-08-02",
+	//   "title": "Prey",
+	//   "video": false,
+	//   "vote_average": 8.1,
+	//   "vote_count": 2894
+	// }
+
+	console.log(movies);
 
 	useEffect(() => {
 		const fetchMovies = async (title) => {
 			const data = await fetch(`${API_KEY}&s=${title}`);
 			const movies = await data.json();
-			setMovies(movies.Search);
+			setMovies(movies.results);
 		};
 
 		fetchMovies(movieCharacter);
@@ -34,7 +47,7 @@ const App = () => {
 			{movies.length > 0 ? (
 				<main className="container movies grid">
 					{movies?.map((movie) => (
-						<Movie key={movie.imdbID} {...movie} />
+						<Movie key={movie.id} {...movie} />
 					))}
 				</main>
 			) : (
@@ -45,24 +58,6 @@ const App = () => {
 
 			<Footer />
 		</>
-	);
-};
-
-const Movie = ({ Title, Year, Poster }) => {
-	return (
-		<article className="movie slide-in-fwd-center">
-			<div className="movie__info flex items-center justify-between">
-				<h1 className="movie__title">{Title?.split(':')[0]}</h1>
-				<p>{Year}</p>
-			</div>
-			<div>
-				<img
-					src={Poster !== 'N/A' ? Poster : 'https://via.placeholder.com/800'}
-					alt={Title}
-					className="movie__image"
-				/>
-			</div>
-		</article>
 	);
 };
 
---
+++ src/components/Movie.jsx
@@ -0,0 +1,20 @@
+const Movie = ({ title, release_date, backdrop_path }) => {
+	const posterURL = `https://image.tmdb.org/t/p/w500${backdrop_path}`;
+	return (
+		<article className="movie slide-in-fwd-center">
+			<div className="movie__info flex items-center justify-between">
+				<h1 className="movie__title">{title?.split(':')[0]}</h1>
+				<p>{release_date.split('-')[0]}</p>
+			</div>
+			<div>
+				<img
+					src={posterURL ? posterURL : 'https://via.placeholder.com/800'}
+					alt={title}
+					className="movie__image"
+				/>
+			</div>
+		</article>
+	);
+};
+
+export default Movie;
