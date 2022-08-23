abd35bc19a0269823685d7fef7c2d93b92f9f584
Joseph Maramba <joseph@smartmile.eu>
2022-08-23 08:38:40 +0300

    add attribution to tmdb

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import tmdb from '../../assets/tmdb.svg';
+
 import './Footer.css';
 
 const Footer = () => {
@@ -5,6 +7,9 @@ const Footer = () => {
     <footer className="movie__footer">
       <div className="container flex justify-between">
         <p>Copyright {new Date().getFullYear()}</p>
+        <a href="https://www.themoviedb.org/" target="_blank">
+          <img src={tmdb} alt="" width={45} />
+        </a>
         <p className="movie__footer-developer">
           GitHub:{' '}
           <a href="https://github.com/mamba-dev-KE" target="_blank">
